e196d666dfef1694ae9b078a7aa376af40c012bd
Kiadtiphong Wongmui <122511756+Kiadtiphong307@users.noreply.github.com>
2023-12-08 11:31:58 +0700

    แก้ไจ

--- src/Main.kt
+++ src/Main.kt
@@ -1,3 +1,27 @@
 fun main(args: Array<String>) {
-    println("Hello, World!")
+    // printtext(name ="Boss")
+    // println("The area of the circle is ${circleArea(1.0)}")
+
+    var T :String?= null
+    // T = "Hello"
+    // if(T != null) {
+    // }
+    // else{
+    //     print(T.length)
+    // }
+    // else{
+    //     print("no data ")
+    // }
+    println(T?.length ?: "no data")
+     println(T!!.length)
 }
+
+ //basic function
+// fun printtext(name: String){
+//     println("This name is $name")
+// }
+
+//function find area of circle
+// fun circleArea(radius: Double): = Math.PI * radius * radius
+
+
